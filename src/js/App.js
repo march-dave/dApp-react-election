10b7de92487b498bb2b455fabc5f6baa97d5b085
march-dave <ysangy@gmail.com>
2018-08-26 13:00:42 -0400

    sold image replace method implement

--- src/js/App.js
+++ src/js/App.js
@@ -128,32 +128,34 @@ class App extends React.Component {
   }
 
   loadRealEstates = () => {
-    // App.contracts.RealEstate.deployed().then(function(instance) {
-    //   return instance.getAllBuyers.call();
-    // }).then(function(buyers) {
-    //   for (i = 0; i < buyers.length; i++) {
-    //     if (buyers[i] !== '0x0000000000000000000000000000000000000000') {
-    //       var imgType = $('.panel-realEstate').eq(i).find('img').attr('src').substr(7);
+    this.contracts.deployed().then( (instance) => {
+      return instance.getAllBuyers.call();
+    }).then( (buyers) => {
+      for (i = 0; i < buyers.length; i++) {
+        if (buyers[i] !== '0x0000000000000000000000000000000000000000') {
+          var imgType = $('.panel-realEstate').eq(i).find('img').attr('src').substr(7);
 
-    //       switch(imgType) {
-    //         case 'apartment.jpg':
-    //           $('.panel-realEstate').eq(i).find('img').attr('src', 'images/apartment_sold.jpg')
-    //           break;
-    //         case 'townhouse.jpg':
-    //           $('.panel-realEstate').eq(i).find('img').attr('src', 'images/townhouse_sold.jpg')
-    //           break;
-    //         case 'house.jpg':
-    //           $('.panel-realEstate').eq(i).find('img').attr('src', 'images/house_sold.jpg')
-    //           break;
-    //       }
+          console.log('imgType: ' + imgType);
 
-    //       $('.panel-realEstate').eq(i).find('.btn-buy').text('Sold').attr('disabled', true);
-    //       $('.panel-realEstate').eq(i).find('.btn-buyerInfo').removeAttr('style');
-    //     }
-    //   }
-    // }).catch(function(err) {
-    //   console.log(err.message);
-    // });
+          switch(imgType) {
+            case 'apartment.jpg':
+              $('.panel-realEstate').eq(i).find('img').attr('src', 'images/apartment_sold.jpg')
+              break;
+            case 'townhouse.jpg':
+              $('.panel-realEstate').eq(i).find('img').attr('src', 'images/townhouse_sold.jpg')
+              break;
+            case 'house.jpg':
+              $('.panel-realEstate').eq(i).find('img').attr('src', 'images/house_sold.jpg')
+              break;
+          }
+
+          $('.panel-realEstate').eq(i).find('.btn-buy').text('Sold').attr('disabled', true);
+          $('.panel-realEstate').eq(i).find('.btn-buyerInfo').removeAttr('style');
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    });
   }
 
   BuyRealEstate = () => {
@@ -184,22 +186,22 @@ class App extends React.Component {
     });
   }
 
-  watchEvents() {
-    // TODO: trigger event when vote is counted, not when component renders
-    this.electionInstance.votedEvent({}, {
-      fromBlock: 0,
-      toBlock: 'latest'
-    }).watch((error, event) => {
-      this.setState({ voting: false })
-    })
-  }
+  // watchEvents() {
+  //   // TODO: trigger event when vote is counted, not when component renders
+  //   this.electionInstance.votedEvent({}, {
+  //     fromBlock: 0,
+  //     toBlock: 'latest'
+  //   }).watch((error, event) => {
+  //     this.setState({ voting: false })
+  //   })
+  // }
 
-  castVote(candidateId) {
-    this.setState({ voting: true })
-    this.electionInstance.vote(candidateId, { from: this.state.account }).then((result) =>
-      this.setState({ hasVoted: true })
-    )
-  }
+  // castVote(candidateId) {
+  //   this.setState({ voting: true })
+  //   this.electionInstance.vote(candidateId, { from: this.state.account }).then((result) =>
+  //     this.setState({ hasVoted: true })
+  //   )
+  // }
 
   render() {
     return (
@@ -225,7 +227,7 @@ class App extends React.Component {
             {
               data.map( i => {
                 return (
-                <div className="col-sm-4 card-body">
+                <div className="col-sm-4 card-body panel-realEstate">
                   <img className="card-img-top" src={i.picture} width="240"/>
 
                   <div className="card-body">
