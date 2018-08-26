8f96c407bea3b036f261c0606b32217d00f64d4e
march-dave <ysangy@gmail.com>
2018-08-26 12:23:24 -0400

    method rename

--- src/js/App.js
+++ src/js/App.js
@@ -83,7 +83,7 @@ class App extends React.Component {
     //     this.electionInstance = electionInstance
     //     this.watchEvents()
 
-    //     this.electionInstance.candidatesCount().then((candidatesCount) => {
+    //     this.electionInstance.candidatesCount(). ((candidatesCount) => {
     //       for (var i = 1; i <= candidatesCount; i++) {
     //         this.electionInstance.candidates(i).then((candidate) => {
     //           const candidates = [...this.state.candidates]
@@ -128,10 +128,35 @@ class App extends React.Component {
   }
 
   loadRealEstates = () => {
-    console.log('loadRealEstates');
+    // App.contracts.RealEstate.deployed().then(function(instance) {
+    //   return instance.getAllBuyers.call();
+    // }).then(function(buyers) {
+    //   for (i = 0; i < buyers.length; i++) {
+    //     if (buyers[i] !== '0x0000000000000000000000000000000000000000') {
+    //       var imgType = $('.panel-realEstate').eq(i).find('img').attr('src').substr(7);
+
+    //       switch(imgType) {
+    //         case 'apartment.jpg':
+    //           $('.panel-realEstate').eq(i).find('img').attr('src', 'images/apartment_sold.jpg')
+    //           break;
+    //         case 'townhouse.jpg':
+    //           $('.panel-realEstate').eq(i).find('img').attr('src', 'images/townhouse_sold.jpg')
+    //           break;
+    //         case 'house.jpg':
+    //           $('.panel-realEstate').eq(i).find('img').attr('src', 'images/house_sold.jpg')
+    //           break;
+    //       }
+
+    //       $('.panel-realEstate').eq(i).find('.btn-buy').text('Sold').attr('disabled', true);
+    //       $('.panel-realEstate').eq(i).find('.btn-buyerInfo').removeAttr('style');
+    //     }
+    //   }
+    // }).catch(function(err) {
+    //   console.log(err.message);
+    // });
   }
 
-  buyRealEstate = () => {
+  BuyRealEstate = () => {
 
     let id = $('#id').val();
     let name = $('#name').val();
@@ -240,7 +265,7 @@ class App extends React.Component {
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary" onClick={this.buyRealEstate}>Submit</button>
+                        <button type="button" class="btn btn-primary" onClick={this.BuyRealEstate}>Submit</button>
                     </div>
                 </div>
             </div>
