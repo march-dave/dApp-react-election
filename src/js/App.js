598ee6afd200226a0943c8487d21e2f52cf90fbf
march-dave <ysangy@gmail.com>
2018-08-28 20:23:29 -0400

    up to date

--- src/js/App.js
+++ src/js/App.js
@@ -65,10 +65,10 @@ class App extends React.Component {
     });
 
     $(this.buyerInfoModal).on('show.bs.modal', e => {
-      let id =  $(e.relatedTarget).parent().parent().find('.id').text();
-      // let modalId =  e.relatedTarget.value;
+      // let id =  $(e.relatedTarget).parent().parent().find('.id').text();
+      let id =  e.relatedTarget.value;
       // console.log('show: id ' + id);
-      console.log('show modalId: ' + id);
+      console.log('show e.relatedTarget.value id: ' + e.relatedTarget.value);
       
       this.contracts.deployed().then( instance => {
         return instance.getBuyerInfo.call(id);
